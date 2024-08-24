ccf1e8c2fa0d860e68b8e0e5501de365a880be76
PayalGandhile <payalgandhile80@gmail.com>
2024-08-24 18:51:04 +0530

    new form added

---
+++ login_form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+        <link rel="stylesheet" href="./styles/login.css">
+    </head>
+    <body>
+        <div class="container">
+            <form class="form" action="" method="post">
+                <h3>Login Now</h3>
+                <input type="email" name="email" placeholder="Enter Email" required>
+                <input type="password" name="password" placeholder="Enter Password" required>
+                <button type="submit">Log In</button>
+                <p>Don't have an account? <a href="register_form.php">register now</a></p>
+            </form>
+        </div>
+    </body>
+</html>
+
---
+++ register_form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Register</title>
+        <link rel="stylesheet" href="./styles/login.css">
+    </head>
+    <body>
+        <div class="container">
+            <form class="form" action="" method="post">
+                <h3>Register Now</h3>
+                <input type="text" name="name" placeholder="Enter Username" required>
+                <input type="email" name="email" placeholder="Enter Email" required>
+                <input type="password" name="password" placeholder="Enter Password" required>
+                <input type="password" name="cpassword" placeholder="Confirm Password" required>
+                <button type="submit">Register</button>
+                <p>already have an account? <a href="login_form.php">login now</a></p>
+            </form>
+        </div>
+    </body>
+</html>
+
